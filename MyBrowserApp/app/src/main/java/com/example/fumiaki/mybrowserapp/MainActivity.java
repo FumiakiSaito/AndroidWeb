b7760465320970688adf12584a773bcae4354c25
齋藤史明 <haratairani3000points@gmail.com>
2015-12-16 10:02:45 +0900

    Webview実装まで

---
+++ MyBrowserApp/app/src/main/java/com/example/fumiaki/mybrowserapp/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.fumiaki.mybrowserapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private EditText urlText;
+
+    private static final String INITIAL_WEBSITE = "http://dotinstall.com";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        myWebView = (WebView) findViewById(R.id.myWebView);
+        urlText = (EditText) findViewById(R.id.urlText);
+
+
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                getSupportActionBar().setSubtitle(view.getTitle());
+                urlText.setText(url);
+            }
+        });
+
+        myWebView.loadUrl(INITIAL_WEBSITE);
+
+    }
+
+    public void showWebsite(View view) {
+        String url = urlText.getText().toString().trim();
+        myWebView.loadUrl(url);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (myWebView != null) {
+            myWebView.stopLoading();
+            myWebView.setWebViewClient(null);
+            myWebView.destroy();
+        }
+    }
+}
